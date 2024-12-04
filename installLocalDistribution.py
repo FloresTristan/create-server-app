5a46c06912c6a7c435ba4b6bbc3aad0466d0c489
Adrian Anton Ladia <ladia.adrian@gmail.com>
2024-12-04 11:42:52 +0800

    Added tasks
    Added tasks to automate creating dist folder and installing to local python

---
+++ installLocalDistribution.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+
+def get_latest_file(folder_path):
+    # List all files in the folder
+    files = [
+        os.path.join(folder_path, f) for f in os.listdir(folder_path)
+        if os.path.isfile(os.path.join(folder_path, f))
+    ]
+
+    # If no files are found, return None
+    if not files:
+        return None
+
+    # Get the file with the latest modification time
+    latest_file = max(files, key=os.path.getmtime)
+    return latest_file
+
+
+def getLatestWheel():
+    cwd = os.getcwd()
+    wheelFolder = cwd + '\\dist'
+    latestFile = get_latest_file(wheelFolder)
+    return latestFile
+
+
+command = 'pip install ' + getLatestWheel()
+process = subprocess.run(command, shell=True, check=True, text=True)
---
+++ test/README.md
@@ -0,0 +1,3 @@
+# 
+
+A simple Flask server template with MongoDB integration.
