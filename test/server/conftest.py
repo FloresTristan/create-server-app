d41efde6c1c5bee2bbd6120b08322925dc93b9d9
Adrian Anton Ladia <ladia.adrian@gmail.com>
2024-12-04 13:29:14 +0800

    deleted test

--- test/server/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from AppConfig import AppConfig  # Adjust the import path according to your project structure
-
-
-def pytest_configure(config):
-    app_config = AppConfig()
-
-    if app_config.getIsProductionEnvironment():
-        pytest.exit("Production environment is set. Skipping tests.")
--- test/server/test_functions.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import AppConfig
-
-def test_env():
-    env = AppConfig.Environment()
-    assert env.getEnvironment() in [
-        'localdev', 'localprod', 'clouddev', 'cloudprod', 'localTest', 'cloudTest'
-    ]
-
