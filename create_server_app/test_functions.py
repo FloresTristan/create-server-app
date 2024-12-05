1cd07c668a69a72da16db1b18b0c1aa737da2dd4
Adrian Anton Ladia <ladia.adrian@gmail.com>
2024-12-05 12:52:15 +0800

    added test security

--- create_server_app/test_functions.py
+++ create_server_app/test_functions.py
@@ -1,5 +1,6 @@
 import os
 
+
 def create_test_functions_file(project_name):
     test_content = """import pytest
 import AppConfig
@@ -9,6 +10,12 @@ def test_env():
     assert env.getEnvironment() in [
         'localdev', 'localprod', 'clouddev', 'cloudprod', 'localTest', 'cloudTest'
     ]
+
+if __name__ == '__main__':
+    if AppConfig().getIsProductionEnvironment():
+        raise Exception("Cannot run tests in production environment.")
+        
+    #INSERT FUNCTIONS BELOW#
 
 """
     with open(os.path.join(project_name, 'test_functions.py'), 'w') as f:
